52abce060d41408c03e1077c16afc168928234a7
Holworth <kangqihan17@mails.ucas.ac.cn>
2020-10-20 14:36:59 +0800

    modified

---
+++ SimpleMalloc/bench.sh
@@ -0,0 +1,18 @@
+make
+rm ./report/smalloc.txt
+echo "[Simple Malloc]" >> ./report/smalloc.txt
+echo -e "\n[Thread Num = 1 Malloc Times=100000]" >> ./report/smalloc.txt
+./main --malloc_type=0 --thread_num=1 --malloc_times=1000000
+echo -e "\n[Thread Num = 4 Malloc Times=100000]" >> ./report/smalloc.txt
+./main --malloc_type=0 --thread_num=4 --malloc_times=1000000
+echo -e "\n[Thread Num = 8 Malloc Times=100000]" >> ./report/smalloc.txt
+./main --malloc_type=0 --thread_num=8 --malloc_times=1000000
+echo -e "\n[Thread Num = 16 Malloc Times=100000]" >> ./report/smalloc.txt
+./main --malloc_type=0 --thread_num=16 --malloc_times=1000000
+echo -e "\n[Thread Num = 32 Malloc Times=100000]" >> ./report/smalloc.txt
+./main --malloc_type=0 --thread_num=32 --malloc_times=1000000
+echo -e "\n\n[Finish Simple Malloc Bench]"
+
+ 
+
+
--- SimpleMalloc/m_example.sh
+++
@@ -1,1 +0,0 @@
-./main --malloc_type=1 --thread_num=4 --malloc_times=100000
